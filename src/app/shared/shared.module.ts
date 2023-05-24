73a498c638f15951a73acf926a48e43b5d711f19
bitz161 <bitzgarcia1996@gmail.com>
2023-05-24 10:08:01 +0800

    fix masking but error is still showing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidebarButtonComponent } from './sidebar-button/sidebar-button.component';
 import { InputSharedComponent } from './input-shared/input-shared.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { NgxMaskDirective, provideEnvironmentNgxMask } from 'ngx-mask';
 
 @NgModule({
   declarations: [SidebarButtonComponent, InputSharedComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
   exports: [SidebarButtonComponent, InputSharedComponent],
   providers: [provideEnvironmentNgxMask()],
 })
